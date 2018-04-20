e14d06c9b254ffdde62d349ab055bcca099b8c39
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-20 16:46:28 +0100

    get artist name function passing test

---
+++ models/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+  attr_reader( :name, :id)
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+  
+end
---
+++ models/specs/artist_spec.rb
@@ -0,0 +1,14 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../artist.rb")
+
+class TestArtist < MiniTest::Test
+  def setup()
+    @artist = Artist.new('name')
+  end
+
+  def test_get_artist_name__expecting_name()
+    assert_equal('name', @artist.name)
+  end
+
+end
