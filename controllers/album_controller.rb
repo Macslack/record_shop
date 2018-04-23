636fe6a6c59f6d65cf3183e5de892ba951991854
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-23 14:53:17 +0100

    album view and artist views, create artist working

--- controllers/album_controller.rb
+++ controllers/album_controller.rb
@@ -1,12 +1,26 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('../models/albums.rb')
-
+require_relative('../models/album.rb')
+#index
 get '/albums' do
   @albums = Album.all()
-  erb(:"album/index")
+  erb(:"albums/index")
+end
+#new
+get '/albums/new' do
+  @albums = Album.all()
+  erb(:"albums/new")
 end
 
+
+#create
+post "/albums/new" do
+  album = Album.new(params)
+  album.save()
+  redirect to "/albums"
+end
+
+#show
 get '/albums/:id' do
   @albums = Album.find(params['id'].to_i)
   erb(:"albums/show")
--- controllers/artist_controller.rb
+++ controllers/artist_controller.rb
@@ -1,13 +1,27 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/artist.rb')
-
+#index
 get '/artists' do
   @artists = Artist.all()
   erb(:"artists/index")
 end
 
+#new
+get '/artists/new' do
+  @artist = Artist.all()
+  erb(:"artists/new")
+end
+
+#show
 get '/artists/:id' do
   @artist = Artist.find(params['id'].to_i)
   erb(:"artists/show")
+end
+
+#create
+post "/artists/new" do
+  artist = Artist.new(params)
+  artist.save()
+  redirect to "/artists"
 end
